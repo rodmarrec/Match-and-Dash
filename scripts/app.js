252489af80294a6cb430592bab072b9661416dce
rodmarrec <romarron01@gmail.com>
2020-09-01 10:21:12 -0700

    adds new table(grid) in html

--- scripts/app.js
+++ scripts/app.js
@@ -36,10 +36,11 @@ console.log("It's here, inside your mind...");
 
 //ANCHOR 1: event listener on button
 //button sanity check - 1st easy thing to do
-$("button").on('click', function(){
+ $("button").on('click', function(){
     console.log("==== Get Set, Match! Dash! ======");
-
-});
+    $(".tiles").empty();
+    setUpRound();
+}); 
 // √
 
 // const $singleTile = $("<div class='tile' />"); //create tiles as div
@@ -47,9 +48,9 @@ $("button").on('click', function(){
     //√
     const $tiles = $(".tiles");  //storing the div container to memory
 
-//ANCHOR 2: create tiles
+//ANCHOR 2: create tiles (container in browser)
 const createTiles = function (tilesAmount){
-    console.log(createTiles(10));
+    console.log("creating tiles",tilesAmount);
   
     for(let i = 1; i <= tilesAmount; i++) {
         const $singleTile = $("<div class='tiles' />").css("background-color", applyRandomColor()) //ANCHOR  create square and applyRandomColor 
@@ -61,7 +62,7 @@ const createTiles = function (tilesAmount){
 //ANCHOR 3: function to apply colorsArr randomly on browser
 //REVIEW applyRandomColor fxn  & choose colors
 const applyRandomColor = function(){
-    const colorsArr = ["blue","rgb(20, 100, 100)","rgb(100, 150, 200)","lightskyblue"];
+    const colorsArr = ["lightskyblue","rgb(255, 0, 0)"];
     const randomIndex = Math.floor(Math.random() * colorsArr.length);
     return colorsArr[randomIndex];   
 } 
@@ -85,23 +86,23 @@ let $points = $("#points")
 $points = 0; //
 // √
 
-//REVIEW once working this will need to change so only if matched are points awarded
+//REVIEW once working this will need to change so only if matched are $points awarded
 //ANCHOR 5: point system and values
 
 const realFlip = function(tileGraphic){
     console.log(tileGraphic);
 
-    if(tileGraphic === "rgb(0, 0, 255)"){ //we will need to assign some sort of value to both the custom cards(objects) to use here
-        console.log("It's blue!");
+    if(tileGraphic === "rgb(255, 0, 0)"){ //we will need to assign some sort of value to both the custom cards(objects) to use here
+        console.log("It's red!");
         $points++;
     }else {
-        console.log("It's not blue.");
-        if($points >= 0) $points--; //this line prevents score from dropping below zero
+        console.log("It's not red.");
+        if($points > 0) $points--; //this line prevents score from dropping below zero
     }
-    $points.text(`Total Points: ${$points}`);
+    $("h2").text(`Total Points: ${$points}`);
 }
 
-$tiles.on("click", ".tiles", doFlip); //REVIEW what does this do exactly?
+$(".tiles").on("click", (".tiles"), doFlip); //REVIEW what does this do exactly?
 
 let $round = $("#round")
 $round = 0; //
@@ -114,7 +115,7 @@ const setTimer = function(){ //seTimer function not being invoked
             clearInterval(timer); //clearInterval() also built into js. and will stop timer
             $round++;
             $("#round").text(`Round ${$round}`); //calls on id round in html
-            timer = 31 - $round; //subtract round number from time
+            timer = 20 - ($round*2); //subtract round number from time
  
             // if(round <= 4) {
             //     setUpRound(); //these 4 lines will countdown to zero, and when at zero, the round will increase, time will be reset to 10, setUpRound fxn will start
@@ -131,14 +132,14 @@ const setTimer = function(){ //seTimer function not being invoked
     }, 1000); //js works in milliseconds
 }
 
-setTimer();//FIXME setTimer to only run on click event on button
+// setTimer();//FIXME setTimer to only run on click event on button
 // √
 
 //ANCHOR Step 7: Functionality
 //depending on the round, setUpRound() generates more tiles
 
 const setUpRound = function() {
-    $tiles.empty();
+    $(".tiles").empty();
     setTimer();
     if($round === 1) {
         createTiles(24);
@@ -147,6 +148,7 @@ const setUpRound = function() {
     }else if($round === 5) {
         createTiles(48);
 }else{
-        createTiles(60)
+        createTiles(50);
+        console.log("game stopped")
 }
-}
+} 
